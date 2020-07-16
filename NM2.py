8a7e58c8c26621183767de281175fc20f50b32ce
BJuyoung <47097563+BJuyoung@users.noreply.github.com>
2020-07-16 16:57:23 +0900

    acmicpc_15651

---
+++ NM2.py
@@ -0,0 +1,23 @@
+def Permutation(index, N, M):
+    if index == M:
+        for i in range(M):
+            print(result[i], end = " ")
+        print()
+        return 0
+
+    for i in range(1,N+1):
+        if index != 0 and result[index-1] > i:
+            continue
+        if visited[i] is not True:
+            result[index] = i
+            visited[i] = True
+            Permutation(index+1, N, M)
+            visited[i] = False # backtracking
+
+
+N, M = map(int, input().split())
+
+visited = [False for i in range(N+1)]
+result = [0 for i in range(M)]
+
+Permutation(0, N, M)
