1d2d1fc9674d7894f1f985c069b165788acf9a28
BJuyoung <47097563+BJuyoung@users.noreply.github.com>
2020-07-22 00:13:33 +0900

    acmicpc_15665

---
+++ NM11.py
@@ -0,0 +1,28 @@
+def Permutation(index, start, M):
+    global result
+    global N
+
+    if index == M:
+        result_str = ' '.join(map(str, result))
+        if result_str not in result_set:
+            print(result_str)
+            result_set.add(result_str)
+        return 0
+
+    for j in range(start, N+1):
+        result[index] = visited[j][0]
+        Permutation(index + 1, j, M)
+
+
+
+N, M = map(int, input().split())
+node = list(map(int, input().split()))
+node.sort()
+visited = {}
+result_set = set()
+
+for i in range(1, N+1):
+    visited[i] = [node[i-1], False]
+
+result = [0 for i in range(M)]
+Permutation(0, 1, M)
