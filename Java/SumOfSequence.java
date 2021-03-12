8abc229c433d929f45f2f67df50c21fa0b7b8d97
BJuyoung <qkdwn0313@gmail.com>
2021-03-12 12:14:21 +0900

    problem_2003

---
+++ Java/SumOfSequence.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SumOfSequence {
+    static int[] sequence;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int sequenceCount = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine(), " ");
+        sequence = new int[sequenceCount];
+        for(int i=0; i<sequenceCount; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int result = matchTarget(target);
+        System.out.println(result);
+    }
+
+    public static int matchTarget(int target){
+        int left = 0;
+        int right = 1;
+
+        int curTotal = 0;
+        int count = 0;
+
+        curTotal += sequence[0];
+        while(left <= right) {
+            if(curTotal == target) {
+                count++;
+                curTotal -= sequence[left];
+                left++;
+            }
+            else if(curTotal < target) {
+                if(right >= sequence.length) break;
+
+                curTotal += sequence[right];
+                right++;
+            }
+            else {
+                curTotal -= sequence[left];
+                left++;
+            }
+        }
+
+        return count;
+    }
+}
