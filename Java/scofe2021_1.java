4ad1a8293aa1aad0431fc2da195d4a9d7febc10b
BJuyoung <qkdwn0313@gmail.com>
2021-03-21 16:32:32 +0900

    scofe2021

---
+++ Java/scofe2021_1.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+//대여 시간을 추천해드립니다
+class scofe2021_1 {
+    public static class Times implements Comparable<Times>{
+        int startTime;
+        int endTime;
+
+        public Times(int s, int e) {
+            this.startTime = s;
+            this.endTime = e;
+        }
+
+        @Override
+        public int compareTo(Times o) {
+            return (Integer.compare(this.endTime, o.endTime));
+        }
+
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Times> pq = new PriorityQueue<>();
+        for(int i=0; i<N; i++) {
+            String[] input = br.readLine().split("");
+            int startTime = (Integer.parseInt(input[0])*10 + Integer.parseInt(input[1]))*60
+                    + (Integer.parseInt(input[3])*10 + Integer.parseInt(input[4]));
+            int endTime = (Integer.parseInt(input[8])*10 + Integer.parseInt(input[9]))*60
+                    + (Integer.parseInt(input[11])*10 + Integer.parseInt(input[12]));
+
+            pq.add(new Times(startTime, endTime));
+        }
+
+        Times curTime = pq.poll();
+        int minEnd = curTime.endTime;
+        int maxStartTime = curTime.startTime;
+        int cnt = 1;
+        while(!pq.isEmpty()) {
+            curTime = pq.poll();
+            if(curTime.startTime <= minEnd && curTime.endTime >= minEnd) cnt++;
+            maxStartTime = Math.max(maxStartTime, curTime.startTime);
+        }
+
+        if(cnt == N) {
+            System.out.printf("%02d:%02d ~ %02d:%02d", maxStartTime/60, maxStartTime%60, minEnd/60, minEnd%60);
+        }
+        else System.out.println(-1);
+    }
+}
---
+++ Java/scofe2021_6.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+//팝업스토어
+class scofe2021_6 {
+    public static class Point implements Comparable<Point>{
+        int x;
+        int y;
+
+        public Point(int curX, int curY) {
+            this.x = curX;
+            this.y = curY;
+        }
+
+        @Override
+        public int compareTo(Point o) {
+            return Integer.compare(this.x, o.x);
+        }
+    }
+
+    static PriorityQueue<Point> points = new PriorityQueue<>();
+    static int[][] road;
+    static int[][] buy;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        road = new int[M][N];
+        buy = new int[M][N];
+
+        for(int[] b : buy)
+            Arrays.fill(b, 0);
+
+        for(int i=0; i<M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0; j<N; j++) {
+                road[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        buy[0][0] = road[0][0];
+        points.add(new Point(0, 0));
+        Search(M, N);
+
+        System.out.println(buy[M-1][N-1]);
+    }
+
+    public static void Search(int M, int N) {
+        while(!points.isEmpty()) {
+            Point p = points.poll();
+
+            if(p.x+1 > 0 && p.x+1 < M) {
+                int total = buy[p.x][p.y] + road[p.x+1][p.y];
+                if(total > buy[p.x+1][p.y]) {
+                    buy[p.x+1][p.y] = total;
+                    points.add(new Point(p.x+1, p.y));
+                }
+            }
+
+            if(p.y+1 > 0 && p.y+1 < N) {
+                int total = buy[p.x][p.y] + road[p.x][p.y+1];
+                if(total > buy[p.x][p.y+1]) {
+                    buy[p.x][p.y+1] = total;
+                    points.add(new Point(p.x, p.y+1));
+                }
+            }
+        }
+
+    }
+}
