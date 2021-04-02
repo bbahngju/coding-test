17781b0a85e47b1697a60849c8600befee2ba404
BJuyoung <qkdwn0313@gmail.com>
2021-04-02 18:37:05 +0900

    problem_19642_not_finished

--- Java/TORIE.java
+++ Java/TORIE.java
@@ -6,12 +6,15 @@ public class TORIE {
     static ArrayList<Integer>[] tree;
     static ArrayList<String> word;
     static boolean[] visited;
+    static boolean[] blocked;
+    static String keyword;
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] arg) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String input = br.readLine().toString();
-        String keyword = br.readLine().toString();
+        String input = br.readLine();
+        keyword = br.readLine();
 
         tree = new ArrayList[input.length()+1];
         for(int i=0; i<input.length()+1; i++) {
@@ -19,10 +22,23 @@ public class TORIE {
         }
         word = new ArrayList<>();
         word.add(0, "0");
+
         visited = new boolean[input.length()+1];
         Init(input, 1);
+        blocked = new boolean[word.size()];
+        for(int i=0; i<word.size(); i++) {
+            System.out.println(word.get(i));
+        }
+        for(int i=1; i<word.size(); i++){
+            Begin(i);
+        }
 
-        Search(1, keyword, 0);
+        if(Print(1)) bw.write("torie!\n");
+        else bw.write(sb.toString());
+
+        bw.flush();
+        br.close();
+        bw.close();
     }
 
     static int f = 1;
@@ -30,7 +46,6 @@ public class TORIE {
         int first = f;
         for(int i=idx; i<input.length(); i++) {
             if(visited[i]) continue;
-
             visited[i] = true;
             if(input.charAt(i) == '[') {
                 if(input.charAt(i-1) != ']')
@@ -48,7 +63,67 @@ public class TORIE {
 
     }
 
-    public static void Search(int node, String key, int keyIdx) {
+    public static void Begin(int curNode) {
+        for(int i=0; i<word.get(curNode).length(); i++){
+            int j;
 
+            boolean check = true;
+            for(j=0; j<keyword.length() && i+j<word.get(curNode).length(); j++) {
+                if(keyword.charAt(j) != word.get(curNode).charAt(i+j)) {
+                    check = false;
+                    break;
+                }
+            }
+
+            if(check) {
+                if(keyword.length() == j) {
+                    blocked[curNode] = true;
+                    break;
+                }
+                boolean result;
+                for(int t : tree[curNode]) {
+                    result = Down(t, j);
+                    if(result) {
+                        blocked[curNode] = true;
+                        break;
+                    }
+                }
+
+            }
+
+            if(blocked[curNode]) break;
+        }
+    }
+
+    public static boolean Down(int curNode, int keyIdx) {
+        boolean check = true;
+        for(int i=0; i<word.get(curNode).length(); i++) {
+               if(word.get(curNode).charAt(i) != keyword.charAt(keyIdx)) {
+                   check = false;
+                   break;
+               }
+               ++keyIdx;
+        }
+        if(!check) return false;
+        if(keyword.length() == keyIdx) return true;
+        for(int t : tree[curNode]) {
+            if(Down(t, keyIdx))
+                return true;
+        }
+        return false;
+    }
+
+    static boolean torie = true;
+    public static boolean Print(int curNode) {
+        if(blocked[curNode]) return torie;
+
+        torie = false;
+        sb.append("["+word.get(curNode));
+        for(int t : tree[curNode]){
+            if(!blocked[t]) Print(t);
+        }
+        sb.append("]");
+
+        return torie;
     }
 }
