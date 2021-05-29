204be9d401c4a9d727967211808f855614969f2c
Juyoung Bahng <qkdwn0313@gmail.com>
2021-05-29 22:24:31 +0900

    Add solution of 2933

--- Java/BOJ/Mineral.java
+++ Java/BOJ/Mineral.java
@@ -1,21 +1,11 @@
 package BOJ;
 
 //BOJ 2933
-
-import com.sun.source.doctree.ThrowsTree;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+//시뮬, 80분
+import java.io.*;
+import java.util.*;
 
 public class Mineral {
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
-    static Queue<Point> points = new LinkedList<>();
-
     public static class Point {
         int x;
         int y;
@@ -24,158 +14,163 @@ public class Mineral {
             this.y = y;
         }
     }
+
+    public static int[] dx = {0, 0, 1, -1};
+    public static int[] dy = {1, -1, 0, 0};
+
     public static void main(String[] arg) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int R = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-        char[][] cave = new char[R][C];
+        int row = Integer.parseInt(st.nextToken());
+        int col = Integer.parseInt(st.nextToken());
 
-        for(int i=0; i<R; i++) {
+        char[][] cave = new char[row][col];
+
+        for(int i = 0; i < row; i++) {
             cave[i] = br.readLine().toCharArray();
         }
 
         int cnt = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
 
-        for(int i=0; i<cnt; i++) {
-            boolean isThrow = isThrowStick(i, Integer.parseInt(st.nextToken()), cave);
-            if(isThrow) searchCluster(cave);
-
-            for (char[] chars : cave) {
-                for (int k = 0; k < cave[0].length; k++) {
-                    System.out.print(chars[k]);
+        int high;
+        boolean success;
+        boolean[][] visited;
+        for(int i = 0; i < cnt; i++) {
+            high = Integer.parseInt(st.nextToken());
+            success = attack(cave, i, high);
+            if(success) {
+                visited = new boolean[row][col]; //매 횟수마다 초기화하는 것.
+                for(int j = 0; j < col; j++) {
+                    if(cave[row-1][j] == 'x' && !visited[row-1][j]) {
+                        bfs(row-1, j, cave, visited);
+                    }
                 }
-                System.out.println();
-            }
-        }
 
-
-    }
-
-    public static boolean isThrowStick(int turn, int hight, char[][] cave) {
-        System.out.println("isThrowStick");
-        int x = cave.length - hight;
-        int nextX, nextY;
-        if(turn % 2 == 0) {
-            for(int i=0; i<cave[0].length; i++) {
-                if(cave[x][i] == 'x') {
-                    cave[x][i] = '.';
-                    addQueue(x, i, cave);
-                    return true;
-                }
-            }
-        }
-        else {
-            for(int i=cave.length-1; i>=0; i--) {
-                if(cave[x][i] == 'x') {
-                    cave[x][i] = '.';
-                    addQueue(x, i, cave);
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-    public static void addQueue(int x, int y, char[][] cave) {
-        System.out.printf("addQueue: %d, %d\n", x, y);
-        points.clear();
-        int nextX, nextY;
-        for(int i=0; i<4; i++) {
-            nextX = x + dx[i];
-            nextY = y + dy[i];
-
-            if(nextX >= 0 && nextX < cave.length && nextY >= 0 && nextY < cave[0].length) {
-                if(cave[nextX][nextY] == 'x') {
-                    points.add(new Point(nextX, nextY));
-
-                }
-            }
-        }
-    }
-    public static void searchCluster(char[][] cave) {
-        System.out.println("searchCluster");
-        boolean[][] visited = new boolean[cave.length][cave[0].length];
-        int x = cave.length-1;
-        boolean isFallen = false;
-
-        while(!points.isEmpty()) {
-            Point p = points.poll();
-            if(!visited[p.x][p.y]) {
-                visited[p.x][p.y] = true;
-                Queue<Point> fallenQ = bfs(p.x, p.y, visited, cave);
-                if(!fallenQ.isEmpty()) {
-                    int minFall = fallenCluster(visited, cave, fallenQ);
-                    reDraw(fallenQ, cave, minFall);
-                    break; //하나의 클러스터만 떨어진다 했으므로.
-                }
-            }
-        }
-    }
-    public static Queue<Point> bfs(int x, int y, boolean[][] v, char[][] cave) {
-        System.out.println("bfs");
-        int nextX, nextY;
-        Queue<Point> save = new LinkedList<>();
-        Queue<Point> search = new LinkedList<>();
-        search.add(new Point(x, y));
-        save.add(new Point(x, y));
-
-        while(!search.isEmpty()) {
-            Point p = search.poll();
-
-            if(p.x == cave.length-1) {
-                save.clear();
-                return save;
-            }
-
-            for(int i=0; i<4; i++) {
-                nextX = p.x + dx[i];
-                nextY = p.y + dy[i];
-
-                if(nextX >= 0 && nextX < cave.length && nextY >= 0 && nextY < cave[0].length) {
-                    if(cave[nextX][nextY] == 'x' && !v[nextX][nextY]) {
-                        v[nextX][nextY] = true;
-                        save.add(new Point(nextX, nextY));
-                        search.add(new Point(nextX, nextY));
+                for(int j = row-1; j >= 0; j--) {
+                    for(int k = 0; k < col; k++) {
+                        if(cave[j][k] == 'x' && !visited[j][k]) {
+                            fallMineral(row, col, j, k, cave, visited);
+                        }
                     }
                 }
             }
+        }
 
-        }
-        return save;
-    }
-    public static int fallenCluster(boolean[][] visited, char[][] cave, Queue<Point> fallen) {
-        System.out.println("fallenCluster");
-        System.out.println(fallen.size());
-        int minDist = 101;
-        int cnt;
-        for(int i=0; i<fallen.size(); i++) {
-            Point p = fallen.poll();
-            cnt = 0;
-            for(int j=p.x; j<cave.length-1; j++) {
-                if(cave[j][p.y] == 'x' && visited[j][p.y]) {
-                    cnt = 101;
-                    break;
-                }
-                if(cave[j][p.y] == 'x' && !visited[j][p.y]) {
-                    break;
-                }
-                cnt++;
+        for(char[] c : cave) {
+            for(char answer : c) {
+                bw.write(answer+"");
             }
-            fallen.add(new Point(p.x, p.y));
-            minDist = Math.min(minDist, cnt);
+            bw.write("\n");
         }
-        System.out.println(minDist);
-        return minDist;
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
-    public static void reDraw(Queue<Point> q, char[][] cave, int minFall) {
-        System.out.println("reDraw");
+
+    public static boolean attack(char[][] cave, int turn, int hight) {
+        int r = cave.length - hight;
+        int c = cave[0].length;
+
+        boolean shot = false;
+        if(turn % 2 == 0) {
+            for(int i = 0; i < c; i++) {
+                if(cave[r][i] == 'x') {
+                    cave[r][i] = '.';
+                    shot = true;
+                    break;
+                }
+            }
+        }
+
+        else {
+            for(int i = c-1; i >= 0; i--) {
+                if(cave[r][i] == 'x') {
+                    cave[r][i] = '.';
+                    shot = true;
+                    break;
+                }
+            }
+        }
+
+        return shot;
+    }
+
+    public static void bfs(int x, int y, char[][] cave, boolean[][] v) {
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x, y));
+        v[x][y] = true;
+
+        int r = cave.length;
+        int c = cave[0].length;
+        int nextX, nextY;
         while(!q.isEmpty()) {
-            Point p = q.poll();
-            cave[p.x][p.y] = '.';
-            cave[p.x][p.y+minFall] = 'x';
+            Point cur = q.poll();
+            for(int i = 0; i < 4; i++) {
+                nextX = cur.x + dx[i];
+                nextY = cur.y + dy[i];
+                if(isAble(r, c, nextX, nextY) && cave[nextX][nextY] == 'x' && !v[nextX][nextY]) {
+                    q.add(new Point(nextX, nextY));
+                    v[nextX][nextY] = true;
+                }
+            }
+        }
+    }
+
+    public static boolean isAble(int r, int c, int x, int y) {
+        return (x >= 0 && x < r && y >= 0 && y < c);
+    }
+
+    public static void fallMineral(int r, int c, int x, int y, char[][] cave, boolean[][] v) {
+        Queue<Point> q = new LinkedList<>();
+        PriorityQueue<Point> save = new PriorityQueue<>((o1, o2) -> o2.x - o1.x);
+
+        q.add(new Point(x, y));
+        save.add(new Point(x, y));
+
+        boolean[][] tmpV = new boolean[r][c];
+        tmpV[x][y] = true;
+
+        int minDist = 101, cntDist;
+        int nextX, nextY;
+        boolean isCount;
+        while(!q.isEmpty()) {
+            Point cur = q.poll();
+            cntDist = 0;
+            isCount = true;
+            for(int i = cur.x+1; i < r; i++) {
+                if(cave[i][cur.y] == 'x' && !v[i][cur.y]) {
+                    isCount = false;
+                    break;
+                }
+                if(cave[i][cur.y] == 'x' && v[i][cur.y]) {
+                    break;
+                }
+
+                cntDist++;
+            }
+
+            if(isCount) minDist = Math.min(minDist, cntDist);
+
+            for(int i = 0; i < 4; i++) {
+                nextX = cur.x + dx[i];
+                nextY = cur.y + dy[i];
+                if(isAble(r, c, nextX, nextY) && cave[nextX][nextY] == 'x' && !tmpV[nextX][nextY]) {
+                    q.add(new Point(nextX, nextY));
+                    save.add(new Point(nextX, nextY));
+                    tmpV[nextX][nextY] = true;
+                }
+            }
+        }
+
+        while(!save.isEmpty()) {
+            Point s = save.poll();
+            cave[s.x][s.y] = '.';
+            cave[s.x + minDist][s.y] = 'x';
+            v[s.x + minDist][s.y] = true;
         }
     }
 }
